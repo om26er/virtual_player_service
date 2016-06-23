648ac30ba4f32e82cabe6ce9624e714cac62ba65
Omer Akram <om26er@gmail.com>
2016-06-24 04:23:29 +0500

    fix url

--- player/urls.py
+++ player/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         player_views.UserRegistrationView.as_view()
     ),
     url(
-        r'^api/users/request_activation_key',
+        r'^api/users/request_activation_key$',
         simple_login_views.RequestActivationKey.as_view(user_model=User)
     ),
     url(
