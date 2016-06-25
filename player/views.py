0df4bad0ba1d7cc6ab019e590eac8d2df99ad40e
Omer Akram <om26er@gmail.com>
2016-06-25 17:19:43 +0500

    Make PUT api equivalent to PATCH

--- player/views.py
+++ player/views.py
@@ -1,7 +1,5 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    RetrieveUpdateDestroyAPIView
-)
+from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from rest_framework import permissions, status
 from rest_framework.response import Response
 
@@ -12,24 +10,37 @@ class UserRegistrationView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class RetrieveUpdateDestroyProfile(RetrieveUpdateDestroyAPIView):
+class RetrieveUpdateDestroyProfile(APIView):
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = UserSerializer
-    http_method_names = ['patch', 'get', 'delete']
+    http_method_names = ['put', 'get', 'delete']
 
-    def get_queryset(self):
-        return self.request.user
+    def get(self, *args, **kwargs):
+        serializer = UserSerializer(instance=self.request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def get_object(self):
-        return self.request.user
+    def delete(self, *args, **kwargs):
+        self.request.user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def update(self, request, *args, **kwargs):
-        # If the request included an email change request,
-        # reply with 403.
-        email = request.data.get('email')
+    def put(self, *args, **kwargs):
+        email = self.request.data.get('email')
         if email:
             return Response(
-                {'email': 'Cannot be changed.'},
-                status=status.HTTP_403_FORBIDDEN
+                {'email': 'Not allowed to change email.'},
+                status=status.HTTP_400_BAD_REQUEST
             )
-        return super().update(request, *args, **kwargs)
+        serializer = UserSerializer(
+            instance=self.request.user,
+            data=self.request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # Make sure to hash the password, in case it was changed.
+        password = self.request.data.get('password')
+        if password:
+            self.request.user.set_password(password)
+            self.request.user.save()
+        return Response(status=status.HTTP_200_OK)
